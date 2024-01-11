365928420a62591049a6aefcf262fd4b26bbbba8
Sourav Rane <souravrane112@gmail.com>
2024-01-11 22:41:08 +0530

    portfolio updated

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Terminal Website",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://fkcodes.com",
+  },
+  {
+    title: "Developer Portfolio",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://thehackershideout.com",
+  },
+  {
+    title: "Ai Nerf Gun",
+    imgUrl: "",
+    stack: ["Python", "CSS", "JavaScript"],
+    link: "https://youtube.com",
+  },
+];
