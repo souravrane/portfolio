4174e99da3526d7c8848c9901435ca7e8332619d
Sourav Rane <souravrane112@gmail.com>
2024-01-12 13:45:22 +0530

    Timeline updated

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem";
 
 const Timeline = () => {
   return (
-    <div>Timeline</div>
-  )
-}
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        <h1>Timeline</h1>
+        {timeline.map((item) => (
+          <TimelineItem {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Timeline
+export default Timeline;
---
+++ src/data/timeline.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    year: "Today",
+    title: "Software Engineer at WiseTech Global.",
+    duration: "1 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  },
+  {
+    year: "Today",
+    title: "Software Engineer at Deloitte USI.",
+    duration: "2.8 years",
+    details: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+  },
+  {
+    year: "Today",
+    title: "Data Scientist at Unilever Industries.",
+    duration: "6 months",
+    details: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth,",
+  },
+];
