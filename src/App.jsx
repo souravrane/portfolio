2ba370cc988a504b304dc00e7c097c3734489a0c
Sourav Rane <souravrane112@gmail.com>
2024-01-13 12:05:52 +0530

    footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Intro from "./components/Intro";
 
 function App() {
   return (
-    <div className="max-w-5xl w-11/12 mx-auto">
+    <div className="bg-white text-stone-900 min-h-screen font-inter max-w-5xl w-11/12 mx-auto">
       <Intro />
       <Portfolio />
       <Timeline />
