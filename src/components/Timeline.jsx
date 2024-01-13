d4e0f3f3221e496b96fa27be000ae9acfb3e5073
Sourav Rane <souravrane112@gmail.com>
2024-01-13 12:08:51 +0530

    Title updated

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import timeline from "../data/timeline";
 import TimelineItem from "./TimelineItem";
+import Title from "./Title";
 
 const Timeline = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center my-20">
       <div className="w-full md:w-7/12">
-        <h1>Timeline</h1>
+        <Title>Timeline</Title>
         {timeline.map((item) => (
           <TimelineItem {...item} />
         ))}
